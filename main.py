a5bae51e5ce80ade6cc4eefbfec5fb980b289aa5
millionernew <olegepifanov596@gmail.com>
2026-08-26 21:10:48 +0300

    fix main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-git branch
-cd ~/zk_bot
-git branch
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 
+TOKEN = "8991980936:AAGMYCNcD0fxALcsrnhXAFt8IH87AHtSiX4"
 
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer("🐍 ZK Pride Bot запущен!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
 
